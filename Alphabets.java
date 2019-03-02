912d3eb578320ffcfd85867e080e05f1a0b4068a
sai19krishna <47615828+sai19krishna@users.noreply.github.com>
2019-03-01 18:07:21 -0600

    Create Alphabets.java

---
+++ Alphabets.java
@@ -0,0 +1,14 @@
+public class Alphabets {
+	      public static void main(String args[]){
+	            char ch;
+	            System.out.println("Small Alphabets: ");
+	            for( ch = 'a' ; ch <= 'z' ; ch++ ){
+	               System.out.println(ch);
+	            }           
+	 
+	            System.out.println("Capital Alphabets: ");
+	            for( ch = 'A' ; ch <= 'Z' ; ch++ ){
+	               System.out.println(ch);
+	            }
+	         }
+	}
